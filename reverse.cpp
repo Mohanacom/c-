3fcb9a5bf4236c0a66b05f01091694b2807ec3b9
Mohanapriya <148201215+Mohanacom@users.noreply.github.com>
2024-10-25 16:45:37 +0530

    Add files via upload

---
+++ reverse.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int num=587,reverse=0,rem;
+    // while(num!=0){
+        
+    
+    //   rem = num % 10;
+    //   reverse = reverse * 10 + rem;
+    //   num /= 10;
+    // };
+
+    // cout<<reverse;
+    
+
+
+    return 0;
+}
+    
